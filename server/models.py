31ace6c45c81e6ed24f172e5a2d86c59cfd43e12
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-29 17:41:14 -0600

    updated user and recipe models

--- server/models.py
+++ server/models.py
@@ -7,12 +7,43 @@ from config import db, bcrypt
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+         
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+			self._password_hash, password.encode('utf-8'))
+        
+    def __repr__(self):
+	    return f'<User {self.username}, {self.image_url}, {self.bio}>'
+
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
+    __talbe_args__ = (db.CheckConstraint('length(instructions) >= 50', name='instructions_length'),)
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='recipes')
 
 class UserSchema(Schema):
     pass
